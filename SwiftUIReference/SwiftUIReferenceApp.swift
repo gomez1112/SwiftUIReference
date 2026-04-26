4cae805b7d0b29724dce0a47ce5d7022342d20bd
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-04-26 17:17:16 -0400

    Initial Commit

---
+++ SwiftUIReference/SwiftUIReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIReferenceApp.swift
+//  SwiftUIReference
+//
+//  Created by Gerard Gomez on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
